2835a895ad3ccb1e8641282ef00e4503857f9c3f
秉叡 吳 <kw0526039@gmail.com>
2023-11-23 00:24:19 +0800

    加入專案檔案。

---
+++ functionTest/Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace functionTest
+{
+    partial class FunctionTest
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.languageSelection = new System.Windows.Forms.GroupBox();
+            this.enRadioButton = new System.Windows.Forms.RadioButton();
+            this.chRadioButton = new System.Windows.Forms.RadioButton();
+            this.testGroupBox = new System.Windows.Forms.GroupBox();
+            this.checkUSBButton = new System.Windows.Forms.Button();
+            this.rs422Button = new System.Windows.Forms.Button();
+            this.checkMacsButton = new System.Windows.Forms.Button();
+            this.testListLabel = new System.Windows.Forms.Label();
+            this.listRichTextBox = new System.Windows.Forms.RichTextBox();
+            this.checkAudioButton = new System.Windows.Forms.Button();
+            this.languageSelection.SuspendLayout();
+            this.testGroupBox.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // languageSelection
+            // 
+            this.languageSelection.Controls.Add(this.enRadioButton);
+            this.languageSelection.Controls.Add(this.chRadioButton);
+            this.languageSelection.Font = new System.Drawing.Font("新細明體", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.languageSelection.Location = new System.Drawing.Point(13, 14);
+            this.languageSelection.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.languageSelection.Name = "languageSelection";
+            this.languageSelection.Padding = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.languageSelection.Size = new System.Drawing.Size(259, 187);
+            this.languageSelection.TabIndex = 0;
+            this.languageSelection.TabStop = false;
+            this.languageSelection.Text = "選擇顯示語言:            Select display language:";
+            // 
+            // enRadioButton
+            // 
+            this.enRadioButton.AutoSize = true;
+            this.enRadioButton.Font = new System.Drawing.Font("新細明體", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.enRadioButton.Location = new System.Drawing.Point(6, 109);
+            this.enRadioButton.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.enRadioButton.Name = "enRadioButton";
+            this.enRadioButton.Size = new System.Drawing.Size(82, 23);
+            this.enRadioButton.TabIndex = 1;
+            this.enRadioButton.TabStop = true;
+            this.enRadioButton.Text = "English";
+            this.enRadioButton.UseVisualStyleBackColor = true;
+            this.enRadioButton.Click += new System.EventHandler(this.enRadioButton_Click);
+            // 
+            // chRadioButton
+            // 
+            this.chRadioButton.AutoSize = true;
+            this.chRadioButton.Font = new System.Drawing.Font("新細明體", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.chRadioButton.Location = new System.Drawing.Point(6, 68);
+            this.chRadioButton.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.chRadioButton.Name = "chRadioButton";
+            this.chRadioButton.Size = new System.Drawing.Size(65, 23);
+            this.chRadioButton.TabIndex = 0;
+            this.chRadioButton.TabStop = true;
+            this.chRadioButton.Text = "中文";
+            this.chRadioButton.UseVisualStyleBackColor = true;
+            this.chRadioButton.Click += new System.EventHandler(this.chRadioButton_Click);
+            // 
+            // testGroupBox
+            // 
+            this.testGroupBox.Controls.Add(this.checkAudioButton);
+            this.testGroupBox.Controls.Add(this.checkUSBButton);
+            this.testGroupBox.Controls.Add(this.rs422Button);
+            this.testGroupBox.Controls.Add(this.checkMacsButton);
+            this.testGroupBox.Font = new System.Drawing.Font("新細明體", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.testGroupBox.Location = new System.Drawing.Point(279, 14);
+            this.testGroupBox.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.testGroupBox.Name = "testGroupBox";
+            this.testGroupBox.Padding = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.testGroupBox.Size = new System.Drawing.Size(224, 534);
+            this.testGroupBox.TabIndex = 1;
+            this.testGroupBox.TabStop = false;
+            this.testGroupBox.Text = "測試選單視窗:";
+            this.testGroupBox.Visible = false;
+            // 
+            // checkUSBButton
+            // 
+            this.checkUSBButton.Font = new System.Drawing.Font("新細明體", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.checkUSBButton.Location = new System.Drawing.Point(6, 127);
+            this.checkUSBButton.Name = "checkUSBButton";
+            this.checkUSBButton.Size = new System.Drawing.Size(212, 36);
+            this.checkUSBButton.TabIndex = 2;
+            this.checkUSBButton.Text = "確認USB功能";
+            this.checkUSBButton.UseVisualStyleBackColor = true;
+            this.checkUSBButton.Click += new System.EventHandler(this.checkUSBButton_Click);
+            // 
+            // rs422Button
+            // 
+            this.rs422Button.Font = new System.Drawing.Font("新細明體", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.rs422Button.Location = new System.Drawing.Point(6, 85);
+            this.rs422Button.Name = "rs422Button";
+            this.rs422Button.Size = new System.Drawing.Size(212, 36);
+            this.rs422Button.TabIndex = 1;
+            this.rs422Button.Text = "確認RS422功能";
+            this.rs422Button.UseVisualStyleBackColor = true;
+            this.rs422Button.Click += new System.EventHandler(this.rs422Button_Click);
+            // 
+            // checkMacsButton
+            // 
+            this.checkMacsButton.Font = new System.Drawing.Font("新細明體", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.checkMacsButton.Location = new System.Drawing.Point(6, 43);
+            this.checkMacsButton.Name = "checkMacsButton";
+            this.checkMacsButton.Size = new System.Drawing.Size(212, 36);
+            this.checkMacsButton.TabIndex = 0;
+            this.checkMacsButton.Text = "確認網路位置";
+            this.checkMacsButton.UseVisualStyleBackColor = true;
+            this.checkMacsButton.Click += new System.EventHandler(this.showMacsButton_Click);
+            // 
+            // testListLabel
+            // 
+            this.testListLabel.AutoSize = true;
+            this.testListLabel.Font = new System.Drawing.Font("新細明體", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.testListLabel.Location = new System.Drawing.Point(509, 14);
+            this.testListLabel.Name = "testListLabel";
+            this.testListLabel.Size = new System.Drawing.Size(104, 22);
+            this.testListLabel.TabIndex = 3;
+            this.testListLabel.Text = "測試歷程:";
+            // 
+            // listRichTextBox
+            // 
+            this.listRichTextBox.Location = new System.Drawing.Point(509, 39);
+            this.listRichTextBox.Name = "listRichTextBox";
+            this.listRichTextBox.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.ForcedVertical;
+            this.listRichTextBox.Size = new System.Drawing.Size(263, 509);
+            this.listRichTextBox.TabIndex = 4;
+            this.listRichTextBox.Text = "";
+            // 
+            // checkAudioButton
+            // 
+            this.checkAudioButton.Font = new System.Drawing.Font("新細明體", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.checkAudioButton.Location = new System.Drawing.Point(6, 169);
+            this.checkAudioButton.Name = "checkAudioButton";
+            this.checkAudioButton.Size = new System.Drawing.Size(212, 36);
+            this.checkAudioButton.TabIndex = 3;
+            this.checkAudioButton.Text = "確認影像及音樂功能";
+            this.checkAudioButton.UseVisualStyleBackColor = true;
+            this.checkAudioButton.Click += new System.EventHandler(this.checkAudioButton_Click);
+            // 
+            // FunctionTest
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.listRichTextBox);
+            this.Controls.Add(this.testListLabel);
+            this.Controls.Add(this.testGroupBox);
+            this.Controls.Add(this.languageSelection);
+            this.Font = new System.Drawing.Font("新細明體", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "FunctionTest";
+            this.Text = "FunctionTest";
+            this.languageSelection.ResumeLayout(false);
+            this.languageSelection.PerformLayout();
+            this.testGroupBox.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox languageSelection;
+        private System.Windows.Forms.RadioButton enRadioButton;
+        private System.Windows.Forms.RadioButton chRadioButton;
+        private System.Windows.Forms.GroupBox testGroupBox;
+        private System.Windows.Forms.Button checkMacsButton;
+        private System.Windows.Forms.Label testListLabel;
+        private System.Windows.Forms.RichTextBox listRichTextBox;
+        private System.Windows.Forms.Button rs422Button;
+        private System.Windows.Forms.Button checkUSBButton;
+        private System.Windows.Forms.Button checkAudioButton;
+    }
+}
+
---
+++ functionTest/Form1.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace functionTest
+{
+    public partial class FunctionTest : Form
+    {
+        bool isCh;
+        string checkMACsPath = "00_SHOWMAC.EXE";
+        string checkRS422Path = "01-MySerialTest.exe";
+        string checkUSBPath = "03-USBDeview.exe";
+        string checkAudioPath = "04-MV.mp4";
+
+        public FunctionTest()
+        {
+            InitializeComponent();
+            Initial();
+            
+        }
+
+        private void Initial()
+        {
+            languageSelection.Text = "選擇顯示語言:\r\nSelect Display Language:";
+            testGroupBox.Visible = true;
+        }
+
+        #region 語言及語言切換區塊
+        /// <summary>
+        /// 切換語言
+        /// </summary>
+        private void SwitchLanguage()
+        {
+            if (isCh)
+            {
+                Chinese();
+            }
+            else
+            {
+                English();
+            }
+        }
+
+        private void Chinese()
+        {
+            testGroupBox.Text = "測試選單視窗:";
+            testListLabel.Text = "測試歷程:";
+
+            checkMacsButton.Text = "確認網路位置";
+            rs422Button.Text = "確認RS422功能";
+            checkAudioButton.Text = "確認影像及音樂功能";
+        }
+
+        private void English()
+        {
+            testGroupBox.Text = "Test menu window:";
+            testListLabel.Text = "Test List:";
+
+            checkMacsButton.Text = "Check MACs IP";
+            rs422Button.Text = "Check RS422 Function";
+            checkAudioButton.Text = "Check Audio Function";
+        }
+        #endregion
+
+        /// <summary>
+        /// 執行外部程式
+        /// </summary>
+        /// <param name="filePath">程式位置</param>
+        /// <param name="testTitle">測試標題</param>
+        /// <param name="question">提問</param>
+        /// <param name="button">控制按鈕</param>
+        private void OpenExternalExecutableFile(string filePath, string testTitle, string question, Button button)
+        {
+            Process process = new Process();
+            process.StartInfo.FileName = filePath;
+            process.EnableRaisingEvents = true;
+            process.Exited += (sender, e) =>
+            {
+                DialogResult result = MessageBox.Show(question, testTitle, MessageBoxButtons.YesNoCancel, MessageBoxIcon.Question);
+                if (result == DialogResult.Yes)
+                {
+                    button.BackColor = Color.Green;
+                    listRichTextBox.Text += testTitle + "= PASS\r\n";
+                }
+                else
+                {
+                    button.BackColor= Color.Red;
+                    listRichTextBox.Text += testTitle + "= NG\r\n";
+                }
+                KeywordChangeColor("PASS", Color.Green);
+                KeywordChangeColor("NG", Color.Red);
+            };
+
+            try
+            {
+                process.Start();
+            }
+            catch
+            {
+                MessageBox.Show("無法開啟");
+            }
+            process.WaitForExit();
+        }
+
+        /// <summary>
+        /// 關鍵字改顏色
+        /// </summary>
+        /// <param name="keyWord">關鍵字</param>
+        /// <param name="color">顏色</param>
+        private void KeywordChangeColor(string keyWord, Color color)
+        {
+            int startIndex = 0;
+            while (startIndex < listRichTextBox.TextLength)
+            {
+                int wordStartIndex = listRichTextBox.Find(keyWord, startIndex, RichTextBoxFinds.WholeWord);
+
+                if (wordStartIndex != -1)
+                {
+                    listRichTextBox.SelectionStart= wordStartIndex;
+                    listRichTextBox.SelectionLength = keyWord.Length;
+                    listRichTextBox.SelectionColor = color;
+                    startIndex = wordStartIndex + keyWord.Length;
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+
+        private void chRadioButton_Click(object sender, EventArgs e)
+        {
+            isCh = true;
+            SwitchLanguage();
+        }
+
+        private void enRadioButton_Click(object sender, EventArgs e)
+        {
+            isCh = false;
+            SwitchLanguage();
+        }
+
+        private void showMacsButton_Click(object sender, EventArgs e)
+        {
+            if (isCh)
+            {
+                OpenExternalExecutableFile(checkMACsPath, checkMacsButton.Text,
+                    "貼紙是否與該電腦MAC一致?", checkMacsButton);
+            }
+            else
+            {
+                OpenExternalExecutableFile(checkMACsPath, checkMacsButton.Text,
+                    "Is the sticker consistent with the MAC of the computer?", checkMacsButton);
+            }
+        }
+
+        private void rs422Button_Click(object sender, EventArgs e)
+        {
+            if (isCh)
+            {
+                OpenExternalExecutableFile(checkRS422Path, rs422Button.Text,
+                    "RS422功能是否正常?", rs422Button);
+            }
+            else
+            {
+                OpenExternalExecutableFile(checkRS422Path, rs422Button.Text,
+                    "Is the RS422 function normal?", rs422Button);
+            }
+        }
+
+        private void checkUSBButton_Click(object sender, EventArgs e)
+        {
+            if (isCh)
+            {
+                OpenExternalExecutableFile(checkUSBPath, checkUSBButton.Text,
+                    "USB功能及速度是否正常?", checkUSBButton);
+            }
+            else
+            {
+                OpenExternalExecutableFile(checkUSBPath,checkUSBButton.Text,
+                    "Are the USB functions and speed normal?", checkUSBButton);
+            }
+        }
+
+        private void checkAudioButton_Click(object sender, EventArgs e)
+        {
+            if (isCh)
+            {
+                OpenExternalExecutableFile(checkAudioPath, checkAudioButton.Text,
+                    "影像及音樂是否正常?", checkAudioButton);
+            }
+            else
+            {
+                OpenExternalExecutableFile(checkAudioPath, checkAudioButton.Text,
+                    "Are the audio normal?", checkAudioButton);
+            }
+        }
+    }
+}
